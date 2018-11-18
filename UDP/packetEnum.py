8a5dde52b92053db14e8f5442fab56c619422a86
Samy Kamkar <git@samy.pl>
2018-11-18 12:17:47 -0800

    Add EndClientTurnMessage

--- UDP/packetEnum.py
+++ UDP/packetEnum.py
@@ -5,6 +5,7 @@ udp_packet_enum = {
                 # Client Packet
                 15620: 'KeepAlive',
                 17187: 'SectorCommand',
+                17104: 'EndClientTurnMessage',
 
                 # Server Packet
                 26301: 'SectorHearbeat'
